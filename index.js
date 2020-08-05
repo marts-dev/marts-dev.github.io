e958cb444810d7396aec6ae5e6a6354618fef01b
marts-dev <8363978+marts-dev@users.noreply.github.com>
2020-08-05 23:59:58 +0800

    Updated design
    Restructured personal page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+AOS.init();
 class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
@@ -47,9 +48,6 @@ class TypeWriter {
   }
 }
 
-//Init on DOM Load
-document.addEventListener("DOMContentLoaded", init);
-
 //Init App
 function init() {
   const textElement = document.querySelector(".txt-type");
@@ -57,6 +55,46 @@ function init() {
   const wait = JSON.parse(textElement.getAttribute("data-wait"));
   //INit typeWriter
   new TypeWriter(textElement, words, wait);
+  const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Button Listeners
+nextBtn.addEventListener('click', ()=>{
+  if (counter >= carouselImages.length - 1 ) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out'
+  counter++;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', ()=>{
+  if (counter <= 0 ) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out'
+  counter--;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+  if (carouselImages[counter].id === 'lastClone') {
+    carouselSlide.style.transition = 'none'
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+  if (carouselImages[counter].id === 'firstClone') {
+    carouselSlide.style.transition = 'none'
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
 }
 
 const hamburger = {
@@ -70,12 +108,16 @@ function doToggle(e) {
   hamburger.navToggle.classList.toggle("expanded");
   hamburger.nav.classList.toggle("expanded");
 }
-const links = hamburger.nav.querySelectorAll("nav a");
 
-hamburger.navToggle.addEventListener("click", doToggle);
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    hamburger.navToggle.classList.toggle("expanded");
-    hamburger.nav.classList.toggle("expanded");
-  });
-});
+//Init on DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+//const links = hamburger.nav.querySelectorAll("nav a");
+
+//hamburger.navToggle.addEventListener("click", doToggle);
+//links.forEach((link) => {
+//  link.addEventListener("click", () => {
+//    hamburger.navToggle.classList.toggle("expanded");
+//    hamburger.nav.classList.toggle("expanded");
+//  });
+//});
