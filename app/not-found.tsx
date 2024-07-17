470014931e6a50d89afa9750d4a1badab79db9ea
marts-dev <8363978+marts-dev@users.noreply.github.com>
2024-07-18 02:39:06 +0800

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
         className="relative flex flex-col gap-4 items-center justify-center px-4 h-screen bg-transparent"
       >
         <h1 className="text-3xl md:text-7xl font-bold text-gray-700 dark:text-white text-center">404</h1>
-        <h2 className="font-light text-base md:text-4xl text-gray-700 dark:text-neutral-200 py-4">UH OH! You're lost.</h2>
+        <h2 className="font-light text-base md:text-4xl text-gray-700 dark:text-neutral-200 py-4">Compiling...</h2>
         <p className="font-extralight text-base text-center md:text-2xl text-gray-700 dark:text-neutral-200 py-4">The page you are looking for does not exist.
           How you got here is a mystery. But you can click the button below
           to go back to the homepage.
