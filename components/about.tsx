bccd46eadc14a90ec054f8973e2e1d04f243b94d
marts-dev <8363978+marts-dev@users.noreply.github.com>
2025-05-06 06:53:46 +0800

    Fix typo

--- components/about.tsx
+++ components/about.tsx
@@ -10,7 +10,7 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className='relative flex flex-col gap-4 items-center text-center bg-transparent max-w-[45rem] mx-auto scroll-mt-44 text-gray-700 dark:text-white'
+      className='relative flex flex-col gap-4 px-4 items-center text-center bg-transparent max-w-[45rem] mx-auto scroll-mt-44 text-gray-700 dark:text-white'
       initial={{ opacity: 0.0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{
@@ -23,7 +23,7 @@ export default function About() {
     >
       <h2 className='text-3xl font-bold'>About Me</h2>
       <p className='mb-3 text-xl'>
-        I graduted from{" "} <span className="italic">FEU - Institute of Technology</span>{" "} with a degree in Computer Science. My favorite part of programming is the problem-solving aspect. I love the feeling of figuring out how things work and figuring out how to fix them when they break.{" "} <span className="underline">I also enjoy the challenge of learning new technologies and frameworks.</span>
+        I graduated from{" "} <span className="italic">FEU - Institute of Technology</span>{" "} with a degree in Computer Science. My favorite part of programming is the problem-solving aspect. I love the feeling of figuring out how things work and figuring out how to fix them when they break.{" "} <span className="underline">I also enjoy the challenge of learning new technologies and frameworks.</span>
       </p>
       <p className='mb-3 text-xl'>
         <span className="italic">{"When I'm not coding"}</span>, I enjoy playing
